77546e6dc1a76f980e325f191527bebc090764ba
Gaurav Joshi <gaurav.joshi@utdallas.edu>
2022-03-29 23:38:46 -0500

    Updated working solution for question 1

--- Homework-2/Question1/mutualfriends.py
+++ Homework-2/Question1/mutualfriends.py
@@ -15,16 +15,31 @@ def generate_mutual_friends(line):
                 mutual_friend = (friend + ", " + user, set(friendList))
             mutual_friends.append(mutual_friend)
     return mutual_friends
-    
+
+def formatOutput(number_of_friends):
+    pair = number_of_friends[0].split(",")
+    result = [i.lstrip() for i in pair]
+    result = (result[0], result[1])
+    output = "{}\t{}".format(result, number_of_friends[1])
+    return output
+  
 # Read data from HDFS
 text_data = sc.textFile("dbfs:/FileStore/shared_uploads/gauravjoshi0910@gmail.com/mutual.txt")
 
 # lines is a list where each line is a list containing 1 user and their mutual friends  
 lines = text_data.map(lambda x : x.split("\t")).filter(lambda y : len(y) > 1)
+
+# generate mutual friends as key-value pairs
 mutual_friends = lines.flatMap(generate_mutual_friends)
 
-mutual_friends.collect()
+# assemble all the mutual friends from across the clusters
+friend_overlap = mutual_friends.reduceByKey(lambda x, y : x.intersection(y)) 
 
+# Get total number of friends
+number_of_friends = friend_overlap.map(lambda x : (x[0], len(x[1]))).filter(lambda x : x[1] > 0).sortBy(lambda x : x[0])    
 
+# format the output  
+output = number_of_friends.map(formatOutput)
 
-
+# save the file on DBFS
+output.coalesce(1).saveAsTextFile("/FileStore/Output_1")
